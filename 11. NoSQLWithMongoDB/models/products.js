91c8acfd1d495c0727cd7142d391e2e643eb45e8
dannguyenmessi1705 <messiprohy@gmail.com>
2023-07-31 14:07:10 +0700

    Connect to MongoDB

--- 11. NoSQLWithMongoDB/models/products.js
+++ 11. NoSQLWithMongoDB/models/products.js
@@ -1,28 +1,28 @@
-const { DataTypes } = require("sequelize"); // Thêm module DataTypes có chức năng tạo các kiểu dữ liệu cho database từ sequelize
-const sequelize = require("../util/database"); // Thêm module sequelize đã tạo ở file database.js
+// const { DataTypes } = require("sequelize"); // Thêm module DataTypes có chức năng tạo các kiểu dữ liệu cho database từ sequelize
+// const sequelize = require("../util/database"); // Thêm module sequelize đã tạo ở file database.js
 
-// Tạo 1 model có tên là products (khi chạy, MySQL tự chuyển tên thành in thường và thêm 's')
-// có các thuộc tính là id, name, price, description đã tạo sẵn và tự thêm 2 thuộc tính createdAt và updatedAt
-const Product = sequelize.define("products", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    unique: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.CHAR(50),
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-});
+// // Tạo 1 model có tên là products (khi chạy, MySQL tự chuyển tên thành in thường và thêm 's')
+// // có các thuộc tính là id, name, price, description đã tạo sẵn và tự thêm 2 thuộc tính createdAt và updatedAt
+// const Product = sequelize.define("products", {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     unique: true,
+//     primaryKey: true,
+//   },
+//   name: {
+//     type: DataTypes.CHAR(50),
+//     allowNull: false,
+//   },
+//   price: {
+//     type: DataTypes.DOUBLE,
+//     allowNull: false,
+//   },
+//   description: {
+//     type: DataTypes.TEXT,
+//     allowNull: false,
+//   },
+// });
 
-module.exports = Product;
+// module.exports = Product;
--- 11. NoSQLWithMongoDB/util/database.js
+++ 11. NoSQLWithMongoDB/util/database.js
@@ -1,8 +1,37 @@
-const {Sequelize} = require("sequelize"); // Thêm module sequelize vào biến Sequelize (object)
+const { MongoClient, ServerApiVersion } = require("mongodb"); // Nhập vào object lấy từ module mongodb
+const URL =
+  "mongodb+srv://nodejsshop:12345@shop.zjodmkn.mongodb.net/?retryWrites=true&w=majority"; // URL của database
+let _db; // Biến lưu trữ database
+const client = new MongoClient(URL, {
+  serverApi: {
+    version: ServerApiVersion.v1, // Sử dụng phiên bản API 1
+    strict: true, // Chỉ cho phép sử dụng những tính năng mới nhất của API
+    deprecationErrors: true, // Hiển thị lỗi khi sử dụng những tính năng cũ
+  },
+}); // Tạo client kết nối với database
 
-// Tạo 1 connect đến database: new Sequelize(database, user, password, {host: địa chỉ, dialect: tên db})
-const sequelize = new Sequelize("node-shop", "root", "17052002", {
-  host: "localhost",
-  dialect: "mysql",
-});
-module.exports = sequelize;
+const mongoConnect = () => {
+  client
+    .connect() // Kết nối với database
+    .then((result) => {
+      console.log("Connected!");
+      _db = result.db();
+      // console.log(_db)
+    }) // Lưu trữ database vào biến _db
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+}; // Kết nối với database và lưu trữ database vào biến _db
+
+const getDB = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
+}; // Trả về database
+
+module.exports = {
+  mongoConnect,
+  getDB,
+};
