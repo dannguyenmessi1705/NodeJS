a1c809f632e5670f64c8b18cf512369d1e412226
dannguyenmessi1705 <messiprohy@gmail.com>
2023-09-09 00:54:54 +0700

    Replace Promise syntaxs with AsyncAwait syntaxs

---
+++ 025. Async Await/controllers/admin.js
@@ -0,0 +1,322 @@
+const products = require("../models/products");
+const Product = require("../models/products");
+const fs = require("fs");
+
+// {DEFINE THE NUMBER OF PER PAGE} //
+const productOfPage = 6;
+
+// {VALIDATION INPUT} //
+const { validationResult } = require("express-validator");
+
+// {ADD PRODUCT PAGE} //
+const addProduct = async (req, res, next) => {
+  res.render("./admin/editProduct", {
+    title: "Add Product",
+    path: "/admin/add-product",
+    hasFooter: false,
+    editing: false, // Phân biệt với trạng thái Edit vs Add Product
+    error: undefined,
+    errorType: undefined, //  ban đầu chưa có giá trị nào lỗi
+    oldInput: {
+      name: "",
+      price: "",
+      description: "",
+    }, // Lưu lại các giá trị vừa nhập (vì ban đầu không có giá trị nào trong trường cả)
+  });
+};
+
+// {CREAT PRODUCT BY MONGOOSE} //
+const postProduct = async (req, res, next) => {
+  const name = req.body.name;
+  let price = req.body.price;
+  if (price.includes(",")) {
+    // Nếu giá trị nhập vào có dấu "," thì thay thế bằng "."
+    price = price.replace(",", ".");
+  }
+  const image = req.file; // Lấy file từ multer
+  if (!image) {
+    // Nếu không có file thì trả về trang add-product với thông báo lỗi
+    return res.status(422).render("./admin/editProduct", {
+      title: "Add Product",
+      path: "/admin/add-product",
+      hasFooter: false,
+      editing: false,
+      error: undefined,
+      errorType: undefined, //  Xác định trường nào chứa giá trị lỗi
+      oldInput: {
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+      }, // Lưu lại các giá trị vừa nhập
+    });
+  }
+  const description = req.body.description;
+  const userId = req.user._id;
+  // VALIDATION INPUT
+  const errorValidation = validationResult(req);
+  if (!errorValidation.isEmpty()) {
+    console.log(errorValidation.array());
+    const [error] = errorValidation.array();
+    return res.status(422).render("./admin/editProduct", {
+      title: "Add Product",
+      path: "/admin/add-product",
+      hasFooter: false,
+      editing: false,
+      error: error.msg,
+      errorType: error.path, //  Xác định trường nào chứa giá trị lỗi
+      oldInput: {
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+      }, // Lưu lại các giá trị vừa nhập
+    });
+  }
+  try {
+    const product = new Product({
+      name,
+      price,
+      url: image.path, // Lấy đường dẫn của file từ multer (đường dẫn này phải được khai báo dạng tĩnh)
+      description,
+      userId,
+    });
+    const result = await product.save();
+    if (result) {
+      console.log("Created Product");
+      res.redirect("/admin/product");
+    } else {
+      const err = new Error("Create product failed");
+      err.httpStatusCode = 500;
+      throw err;
+    }
+  } catch (err) {
+    // {ERROR MIDDLEWARE} //
+    if (!err.httpStatusCode) {
+      err.httpStatusCode = 500;
+    }
+    next(err);
+  }
+};
+
+// {GET ALL PRODUCTS BY MONGOOSE} //
+const getProduct = async (req, res, next) => {
+  /// {PAGINATION} ///
+  const curPage = +req.query.page || 1; // Lấy giá trị của query 'page' từ URL, nếu không có thì mặc định là 1
+  let numProducts; // Khai báo biến để lưu số lượng sản phẩm
+  try {
+    // {AUTHORIZATION} //
+    const numOfProducts = await Product.countDocuments({
+      userId: req.user._id,
+    }); // Đếm số lượng sản phẩm trong database (userId: req.user._id) - Chỉ đếm số lượng sản phẩm của user đang đăng nhập
+    numProducts = +numOfProducts; // Lưu số lượng sản phẩm vào biến numProducts
+    // {AUTHORIZATION} //
+    const products = await Product.find({ userId: req.user._id }) // Tìm tất cả sản phẩm trong database (userId: req.user._id) - Chỉ tìm sản phẩm của user đang đăng nhập
+      .skip((curPage - 1) * productOfPage) // Bỏ qua các sản phẩm trước sản phẩm hiện tại (curPage - 1) * productOfPage
+      .limit(productOfPage) // Giới hạn số lượng sản phẩm trên 1 trang
+      .select("name price url description _id") // Chỉ lấy các thuộc tính name, price, url, description, bỏ thuộc tính _id
+      .exec(); // Thực thi
+    if (!products) {
+      const err = new Error("Product not found");
+      err.httpStatusCode = 404;
+      throw err;
+    }
+    res.render("./admin/products", {
+      title: "Admin Product",
+      items: products,
+      path: "/admin/product",
+      hasFooter: false,
+      // {PAGINATION} //
+      lastPage: Math.ceil(numProducts / productOfPage), // Tính số lượng trang
+      curPage: curPage, // Trang hiện tại
+      nextPage: curPage + 1, // Trang tiếp theo
+      prevPage: curPage - 1, // Trang trước
+      hasPrevPage: curPage > 1, // Kiểm tra xem có trang trước hay không
+      hasNextPage: curPage < Math.ceil(numProducts / productOfPage), // Kiểm tra xem có trang tiếp theo hay không
+    });
+  } catch (err) {
+    // {ERROR MIDDLEWARE} //
+    if (!err.httpStatusCode) {
+      err.httpStatusCode = 500;
+    }
+    next(err);
+  }
+};
+
+// {GET EDIT PRODUCT BY MONGOOSE} //
+const getEditProduct = async (req, res, next) => {
+  const isEdit = req.query.edit;
+  if (!isEdit) {
+    return res.redirect("/");
+  }
+  const ID = req.params.productID; // // Lấy route động :productID bên routes (URL) - VD: http://localhost:3000/product/0.7834371053383911 => ID = 0.7834371053383911
+  try {
+    const product = await Product.findById(ID);
+    if (product.userId.toString() !== req.user._id.toString()) {
+      // Kiểm tra xem user hiện tại có phải là người tạo ra product này hay không
+      return res.redirect("/admin/product"); // Nếu không phải thì redirect về trang chủ
+    }
+    if (!product) {
+      const err = new Error("Product not found");
+      err.httpStatusCode = 404;
+      throw err;
+    }
+    res.render("./admin/editProduct", {
+      title: "Edit Product",
+      path: "/admin/add-product",
+      hasFooter: false,
+      editing: isEdit, // truyền giá trị của query 'edit' vào biến editing để kiểm tra xem có phải đang ở trạng thái edit hay không
+      item: product, // gán product vừa tìm được vào biến item để đưa vào file ejs
+      error: undefined,
+      errorType: undefined, //  ban đầu chưa có giá trị nào lỗi
+      oldInput: {
+        name: "",
+        price: "",
+        description: "",
+      }, // Lưu lại các giá trị vừa nhập (vì ban đầu không có giá trị nào trong trường cả)
+    });
+  } catch (err) {
+    if (!err.httpStatusCode) {
+      err.httpStatusCode = 500;
+    }
+  }
+};
+
+// {UPDATE PRODUCT BY MONGOOSE} //
+const postEditProduct = async (req, res, next) => {
+  const name = req.body.name;
+  let price = req.body.price;
+  if (price.includes(",")) {
+    // Nếu giá trị nhập vào có dấu "," thì thay thế bằng "."
+    price = price.replace(",", ".");
+  }
+  const image = req.file; // Lấy file từ multer
+  let url = undefined; // Khai báo biến url để lưu đường dẫn của file
+  if (image) {
+    // Nếu có file thì lưu đường dẫn của file vào biến url
+    url = image.path; // Lấy đường dẫn của file từ multer (đường dẫn này phải được khai báo dạng tĩnh)
+  }
+  const description = req.body.description;
+  const ID = req.body.id; // ".id" vì id được đặt trong thuộc tính name của thẻ input đã được hidden
+  // VALIDATION INPUT
+  const errorValidation = validationResult(req);
+  // VALIDATION INPUT
+  if (!errorValidation.isEmpty()) {
+    console.log(errorValidation.array());
+    const [error] = errorValidation.array();
+    return res.status(422).render("./admin/editProduct", {
+      title: "Edit Product",
+      path: "/admin/add-product",
+      hasFooter: false,
+      editing: true, // truyền giá trị của query 'edit' vào biến editing để kiểm tra xem có phải đang ở trạng thái edit hay không
+      item: product,
+      error: error.msg,
+      errorType: error.path, //  Xác định trường nào chứa giá trị lỗi
+      oldInput: {
+        name,
+        price,
+        description,
+      }, // Lưu lại các giá trị vừa nhập
+    });
+  }
+  try {
+    const product = await Product.findById(ID);
+    if (!product) {
+      const err = new Error("Product not found");
+      err.httpStatusCode = 404;
+      throw err;
+    }
+    // {AUTHORIZATION} //
+    if (product.userId.toString() !== req.user._id.toString()) {
+      // Kiểm tra xem user hiện tại có phải là người tạo ra product này hay không
+      return res.redirect("/"); // Nếu không phải thì redirect về trang chủ
+    }
+    // Cập nhật lại các giá trị của product theo req.body
+    product.name = name;
+    product.price = price;
+    if (url) {
+      // Nếu có file thì lưu đường dẫn của file vào biến url
+      fs.unlink(product.url, (err) => {
+        // Xóa file cũ
+        if (err) {
+          // Nếu có lỗi thì in ra lỗi
+          console.log(err);
+          next(new Error(err));
+        }
+      });
+      product.url = url; // Lưu đường dẫn của file mới
+    }
+    product.description = description;
+    // Lưu lại vào database
+    const result = await product.save();
+    if (result) {
+      console.log("Updated!");
+      res.redirect("/admin/product");
+    } else {
+      const err = new Error("Update product failed");
+      err.httpStatusCode = 500;
+      throw err;
+    }
+    // Muốn nhanh hơn thì dùng method findByIdAndUpdate, ruy nhiên dùng save() có thể dùng được với middleware
+  } catch (err) {
+    if (!err.httpStatusCode) {
+      err.httpStatusCode = 500;
+    }
+    next(err);
+  }
+};
+
+// {DELETE PRODUCT BY MONGOOSE} //
+const deleteProduct = async (req, res, next) => {
+  const ID = req.params.productID; // // Lấy route động :productID bên routes (URL) - VD: http://localhost:3000/product/0.7834371053383911 => ID = 0.7834371053383911
+  // Vì không phải load lại trang nên không dùng method POST của form => không thể dùng req.body
+  // Xoá sản phẩm trong database thì phải xoá file ảnh trong folder images (tiết kiệm ổ cứng)
+  let urlDelete; // Khai báo biến url để lưu đường dẫn của file
+  try {
+    const product = await Product.findById(ID);
+    if (!product) {
+      const err = new Error("Product not found");
+      err.httpStatusCode = 404;
+      throw err;
+    }
+    urlDelete = product.url; // Lưu đường dẫn của file vào biến urlDelete
+    // {AUTHORIZATION} //
+    const result = await Product.deleteOne({ _id: ID, userId: req.user._id }); // Kiểm tra xem user hiện tại có phải là người tạo ra product này hay không (userId: req.user._id)
+    if (!result) {
+      const err = new Error("Delete failed");
+      err.httpStatusCode = 404;
+      throw err;
+    }
+    fs.unlink(urlDelete, (err) => {
+      // Xóa file cũ
+      if (err) {
+        // Nếu có lỗi thì in ra lỗi
+        console.log(err);
+        throw err;
+      }
+    });
+    // Xoá cả sản phẩm nếu có trong giỏ hàng
+    req.user.cart.items = req.user.cart.items.filter((item) => {
+      return item.productId.toString() !== ID.toString();
+    });
+    const save = await req.user.save();
+    if (!save) {
+      const err = new Error("Delete failed");
+      err.httpStatusCode = 404;
+      throw err;
+    }
+    console.log("Deleted!");
+    res.status(200).json({ message: "Delete successfull" }); // Trả về kết quả là json với message là Delete successfull
+  } catch (err) {
+    if (!err.httpStatusCode) {
+      err.httpStatusCode = 500;
+    }
+    next(err);
+  }
+};
+module.exports = {
+  addProduct,
+  postProduct,
+  getProduct,
+  getEditProduct,
+  postEditProduct,
+  deleteProduct,
+};
---
+++ 025. Async Await/server.js
@@ -0,0 +1,164 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const rootDir = require("./util/path.js");
+app.use(express.static(path.join(rootDir, "public")));
+
+/* FOR COOKIES ONLY 
+// {SET COOKIE FOR EXPRESS} //
+const cookies = require("cookie-parser"); // q
+app.use(cookies("secret")); // Truyền "secret" để dùng các lệnh mã hoá Cookie
+*/
+
+// {DÙNG MONGODB ĐỂ LƯU TRỮ SESSION} //
+const session = require("express-session"); // Nhập module express-session
+const URL = require("./util/database"); // Nhập vào object lấy URL connect MONGO từ file database.js
+const MongoDBStore = require("connect-mongodb-session")(session); // Nhập module connect-mongodb-session để lưu session vào database
+const storeDB = new MongoDBStore({
+  // Tạo 1 store để lưu session vào database
+  uri: URL, // Đường dẫn kết nối đến database
+  collection: "sessions", // Tên collection để lưu session
+});
+
+// {SET SESSION + COOKIES FOR EXPRESS} //
+// Cấu hình session
+app.use(
+  session({
+    secret: "Nguyen Di Dan", // Chuỗi bí mật để mã hoá session
+    resave: false, // resave: false => Không lưu lại session nếu không có sự thay đổi (Không cần thiết)
+    saveUninitialized: false, // saveUninitialized: false => Không lưu lại session nếu không có sự thay đổi (Không cần thiết)
+    // resave vs saveUninitialized: https://stackoverflow.com/questions/40381401/when-use-saveuninitialized-and-resave-in-express-session
+    store: storeDB, // Lưu session vào database
+    // Ngoài ra có thể tuỳ chỉnh thêm cho cookie: secure, path, signed,... ở cấu hình session
+    cookie: {
+      signed: true,
+    },
+  })
+);
+
+// {CSRF khai báo sau khi định nghĩa SESSION} //
+const Tokens = require("csrf"); // Nhập module csrf
+const csrf = new Tokens(); // Tạo 1 object csrf
+const csrfProtectSecret = csrf.secretSync(); // Tạo 1 secret để mã hoá token
+// {MIDDLEWARE ĐỂ TRUYỀN BIẾN LOCALS CHO TẤT CẢ CÁC ROUTE} //
+app.use((req, res, next) => {
+  const token = csrf.create(csrfProtectSecret); // Tạo 1 token
+  res.locals.authenticate = req.session.isLogin; // Truyền biến authenticate vào locals để sử dụng ở tất cả các route
+  res.locals.csrfToken = token; // Truyền biến csrfToken vào locals để sử dụng ở tất cả các route
+  next();
+}); // Sử dụng middleware bảo vệ các route, nếu không có token thì các lệnh request sẽ báo lỗi
+
+// {BODY PARSER} // (Để lấy dữ liệu từ form) //
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// {MULTER} // (Để lấy dữ liệu file từ form) //
+const multer = require("multer"); // Nhập module multer
+const fileStorage = multer.diskStorage({
+  // Tạo 1 storage để lưu file
+  destination(req, file, callback) {
+    // Định nghĩa đường dẫn lưu file
+    callback(null, "images"); // Lưu file vào folder images
+  },
+  filename(req, file, callback) {
+    // Định nghĩa tên file
+    const date = new Date(); // Lấy ngày giờ hiện tại
+    const formattedDate = date
+      .toISOString()
+      .replace(/:/g, "_")
+      .replace(/\./g, ""); // Định dạng ngày giờ hiện tại (phải chuyển đổi sang dạng string mới đúng cú pháp đặt tên file)
+    callback(null, formattedDate + file.originalname); // Đặt tên file = ngày giờ hiện tại + tên file gốc
+  },
+});
+const fileFilter = (req, file, callback) => {
+  // Định nghĩa loại file được phép upload
+  if (
+    // Nếu file là 1 trong các loại này thì cho phép upload
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    callback(null, true); // true => cho phép upload
+  } else {
+    callback(null, false); // false => không cho phép upload
+  }
+};
+app.use(
+  // Sử dụng middleware multer
+  multer({
+    // Định nghĩa các thuộc tính của multer
+    storage: fileStorage, // Lưu file vào storage đã định nghĩa ở trên
+    fileFilter: fileFilter, // Chỉ cho phép upload các loại file đã định nghĩa ở trên
+  }).single("image") // Chỉ cho phép upload 1 file duy nhất có name="image"
+);
+app.use("/images", express.static(path.join(rootDir, "images"))); // Định nghĩa đường dẫn tĩnh để truy cập vào folder images (để hiển thị hình ảnh đã upload) - Nếu không có dòng này thì hình ảnh sẽ không hiển thị được
+
+// {FLASH MESSAGE} //
+const flash = require("connect-flash");
+app.use(flash());
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// {RUN SERVER + Add user to req (Phân quyền)} //
+const mongoose = require("mongoose"); // Nhập module mongoose
+const User = require("./models/users"); // Nhập vào class User lấy từ file users.js
+mongoose
+  .connect(URL)
+  .then(() => {
+    app.listen(3000);
+    console.log("Connected!");
+  }) // Kết nối với database, sau đó mới chạy server
+  .catch((err) => {
+    console.log(err);
+    res.redirect("/500-maintenance");
+  });
+
+// {MIDDLEWARE PHÂN QUYỀN DÙNG SESSION} //
+app.use(async (req, res, next) => {
+  if (!req.session?.user) {
+    // Nếu không có session user thì return next() để chạy các middleware tiếp theo mà không có phân quyền
+    return next();
+  }
+  try {
+    const user = await User.findById(req.session.user._id); // Tìm kiếm user trong collection users có id trùng với id của session user
+    // Nếu ko tìm thấy user => chuyển hướng mà không có phân quyền
+    if (!user) {
+      next();
+    }
+    // Nếu tìm thấy user thì lưu vào req.user
+    req.user = new User(user); // Lưu lại user vào request để sử dụng ở các middleware tiếp theo (không cần dùng new User vì user đã là object rồi, có thể dùng các method của mongoose cũng như từ class User luôn )
+    // Tuy nhiên, Ko cần req.user nữa vì dùng session rồi (biến user sẽ lưu vào req.session.user)
+    next(); // Tiếp tục chạy các middleware tiếp theo với phân quyền
+  } catch (err) {
+    if (!err.httpStatusCode) {
+      err.httpStatusCode = 500;
+    }
+    next(err);
+  }
+});
+
+// {LOGIN ROUTE} //
+const authRoute = require("./routes/auth");
+const adminRoute = require("./routes/admin");
+const personRoute = require("./routes/user");
+const paymentRoute = require("./routes/payment");
+const errorRoute = require("./routes/error");
+
+app.use("/admin", adminRoute);
+app.use(personRoute);
+// {LOGIN ROUTE} //
+app.use(authRoute);
+app.use(paymentRoute);
+app.use(errorRoute);
+
+// {ERROR MIDDLEWARE} // (Phải đặt ở cuối cùng) // Nếu không có lỗi thì sẽ chạy qua các middleware trước, nếu có lỗi thì sẽ chạy qua middleware này
+app.use((err, req, res, next) => {
+  res.status(500).render("500", {
+    title: "Server maintenance",
+    path: "/500",
+    authenticate: req.session.isLogin, // Vì đây là trang lỗi được ưu tiên thực hiện trước các route khác nên chưa có session, nên phải truyền biến authenticate vào để sử dụng ở header
+  });
+});
+/// !!! Lưu ý: Nếu có lỗi thì phải truyền lỗi vào next() để nó chạy qua middleware này, nếu không thì nó sẽ chạy qua các middleware tiếp theo mà không có lỗi
