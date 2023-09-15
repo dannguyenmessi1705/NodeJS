ad4d97577fcb021b8bd63f860259b88d5e3f300e
dannguyenmessi1705 <messiprohy@gmail.com>
2023-09-15 21:50:45 +0700

    Swagger (API UI)

---
+++ 028. Swagger (API UI)/api/routes/admin.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const route = express.Router();
+const adminController = require("../controllers/admin");
+const ProtectRoute = require("../middleware/isAuth");
+const { verifyCSRFToken } = require("../middleware/csrfToken");
+
+// {TẠO CAC ENDPOINT API} //
+
+// {VALIDATION INPUT} //
+const { check } = require("express-validator");
+
+// {VALIDATION INPUT} //
+route.post(
+  "/add-product",
+  ProtectRoute,
+  verifyCSRFToken,
+  [
+    check("name", "Invallid name").trim().isString().notEmpty(),
+    check("price", "Invalid price").isFloat().notEmpty(),
+    check("description", "Please don't leave the blank description")
+      .trim()
+      .notEmpty(),
+  ],
+  adminController.postProduct
+);
+
+route.get("/products", ProtectRoute, adminController.getProduct);
+
+// {ADD EDIT PRODUCT} //
+// url = "http://.../edit-poduct/id"
+route.get(
+  "/edit-product/:productID",
+  ProtectRoute,
+  adminController.getEditProduct
+);
+
+// {UPDATE PRODUCT}
+// {VALIDATION INPUT} //
+route.put(
+  "/edit-product/:productID",
+  ProtectRoute,
+  verifyCSRFToken,
+  [
+    check("name", "Invallid name").trim().isString().notEmpty(),
+    check("price", "Invalid price").isFloat().notEmpty(),
+    check("description", "Please don't leave the blank description")
+      .trim()
+      .notEmpty(),
+  ],
+  adminController.postEditProduct
+);
+
+// {DELETE PRODUCT} //
+route.delete(
+  "/delete-product/:productID",
+  ProtectRoute,
+  verifyCSRFToken,
+  adminController.deleteProduct
+); // Sửa method xoá sang delete
+
+module.exports = route;
